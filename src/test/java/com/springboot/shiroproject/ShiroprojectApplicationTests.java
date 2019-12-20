8c23c01c1943591d7d02f5527c2186e6f50b8b45
pu_xb <2679862469@qq.com>
2019-12-20 09:20:26 +0800

    first commit

---
+++ src/test/java/com/springboot/shiroproject/ShiroprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.shiroproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
